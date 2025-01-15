4b2fd4803223005b15c0c082b15c6eb935c50fad
Kaley Kwan <kaleykwan@Mac.hsd1.ca.comcast.net>
2025-01-15 09:29:41 -0800

    add npm install to js script

--- scripts/check_js_licenses.js
+++ scripts/check_js_licenses.js
@@ -1,86 +1,4 @@
-// import { init } from "license-checker";
-// import { readdirSync, statSync } from "fs";
-// import { join } from "path";
-
-// const allowedLicenses = [
-//   "MIT",
-//   "Apache-2.0",
-//   "BSD-3-Clause",
-//   "BSD-2-Clause",
-//   "ISC",
-// ];
-
-// function findAllPackageJsonFiles(directory) {
-//   const results = [];
-//   const files = readdirSync(directory);
-
-//   for (const file of files) {
-//     const fullPath = join(directory, file);
-
-//     if (file === "node_modules") {
-//       continue;
-//     }
-
-//     if (statSync(fullPath).isDirectory()) {
-//       results.push(...findAllPackageJsonFiles(fullPath));
-//     } else if (file === "package.json") {
-//       results.push(fullPath);
-//     }
-//   }
-
-//   return results;
-// }
-
-// function checkLicenses(directory) {
-//   return new Promise((resolve, reject) => {
-//     init({ start: directory, json: true }, (err, packages) => {
-//       if (err) {
-//         return reject(err);
-//       }
-
-//       const disallowed = Object.keys(packages).filter((pkg) => {
-//         const license = packages[pkg].licenses;
-//         const licenseList = Array.isArray(license) ? license : [license];
-//         return licenseList.some((lic) => !allowedLicenses.includes(lic));
-//       });
-
-//       if (disallowed.length > 0) {
-//         console.log(`Disallowed licenses found in ${directory}:`);
-//         disallowed.forEach((pkg) => {
-//           console.log(`${pkg}: ${packages[pkg].licenses}`);
-//         });
-//         process.exit(1);
-//       } else {
-//         console.log(`All licenses are allowed in ${directory}!`);
-//       }
-
-//       resolve();
-//     });
-//   });
-// }
-
-// async function main() {
-//   const rootDir = ".";
-//   console.log("Finding all package.json files...");
-//   const packageJsonFiles = findAllPackageJsonFiles(rootDir);
-
-//   console.log("Checking licenses...");
-//   try {
-//     for (const file of packageJsonFiles) {
-//       const dir = join(file, "..");
-//       console.log(`Checking licenses in ${dir}...`);
-//       await checkLicenses(dir);
-//     }
-
-//     console.log("All licenses are allowed across all discovered packages!");
-//   } catch (err) {
-//     console.error("Error:", err);
-//     process.exit(1);
-//   }
-// }
-
-// main();
-
+import { execSync } from "child_process";
 import { init } from "license-checker";
 import { readdirSync, statSync } from "fs";
 import { join } from "path";
@@ -151,10 +69,26 @@ function checkLicenses(directory) {
   });
 }
 
+function runNpmInstall(directory) {
+  try {
+    console.log(`Running npm install in ${directory}...`);
+    execSync("npm install", { cwd: directory, stdio: "inherit" });
+    console.log(`npm install completed in ${directory}.`);
+  } catch (error) {
+    console.error(`Error running npm install in ${directory}:`, error.message);
+  }
+}
+
 async function main() {
   const rootDir = ".";
   console.log("Finding all package.json files...");
   const packageJsonFiles = findAllPackageJsonFiles(rootDir);
+
+  console.log("Running npm install in all directories...");
+  for (const file of packageJsonFiles) {
+    const dir = join(file, "..");
+    runNpmInstall(dir);
+  }
 
   console.log("Checking licenses...");
   const allDisallowedPackages = [];
